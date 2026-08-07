d3d4d26b8617c9089abd41596a8173c290dd0a2b
p94vanshgambhir <vansh.gambhir@joshtechnologygroup.com>
2026-08-07 18:32:30 +0530

    [VG_A1_01]: fixes on navbar, implemented using bem model

--- src/script/index.js
+++ src/script/index.js
@@ -1,18 +1,34 @@
-const hamburger = document.getElementById('hamburger');
-const dialog = document.getElementsByClassName('dialog')[0];
-const category = document.getElementById('category');
-const dialogMdLg = document.getElementsByClassName('dialog-md-lg')[0];
+const hamburger = document.getElementsByClassName(
+    'navbar__hamburger--open-dialog',
+)[0];
+const dialogMobile = document.getElementsByClassName(
+    'navbar__dialog--mobile',
+)[0];
 hamburger.addEventListener('click', () => {
-    if (dialog.hasAttribute('hidden')) {
-        dialog.removeAttribute('hidden');
+    if (dialogMobile.hasAttribute('hidden')) {
+        dialogMobile.removeAttribute('hidden');
+        hamburger.style.background =
+            'linear-gradient(97.54deg, #002f52 35.49%, #326589 165.37%)';
+        hamburger.style.color = 'white';
     } else {
-        dialog.setAttribute('hidden', true);
+        dialogMobile.setAttribute('hidden', 'true');
+        hamburger.style.background = 'white';
+        hamburger.style.color = '#002f52';
     }
 });
+const category = document.getElementsByClassName(
+    'navbar__navigationitem--heading',
+)[0];
+const dialogTab = document.getElementsByClassName('navbar__dialog--tablet')[0];
 category.addEventListener('click', () => {
-    if (dialogMdLg.hasAttribute('hidden')) {
-        dialogMdLg.removeAttribute('hidden');
+    if (dialogTab.hasAttribute('hidden')) {
+        dialogTab.removeAttribute('hidden');
+        category.style.background =
+            'linear-gradient(97.54deg, #002f52 35.49%, #326589 165.37%)';
+        category.style.color = 'white';
     } else {
-        dialogMdLg.setAttribute('hidden', true);
+        dialogTab.setAttribute('hidden', true);
+        category.style.background = 'white';
+        category.style.color = '#002f52';
     }
 });
