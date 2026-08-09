f9949370f38268727a17cb6e35eeb61edd3a3763
p94vanshgambhir <vansh.gambhir@joshtechnologygroup.com>
2026-08-09 16:08:57 +0530

    [VG_A1_03]: Made latest and best section

--- src/script/index.js
+++ src/script/index.js
@@ -1,32 +1,28 @@
-const hamburger = document.getElementsByClassName(
-    'navbar__hamburger--open-dialog',
-)[0];
-const dialogMobile = document.getElementsByClassName(
-    'navbar__dialog--mobile',
-)[0];
+const hamburger = document.getElementById('hamburger');
+const dialogMobile = document.getElementById('dialog_mobile');
+const whiteColor = 'white';
+const darkBlueColor = '#002f52';
+const cyanBlueColor = '#326589';
 hamburger.addEventListener('click', () => {
     if (dialogMobile.hasAttribute('hidden')) {
         dialogMobile.removeAttribute('hidden');
-        hamburger.style.color = 'white';
+        hamburger.style.color = whiteColor;
     } else {
         dialogMobile.setAttribute('hidden', 'true');
-        hamburger.style.background = 'white';
-        hamburger.style.color = '#002f52';
+        hamburger.style.background = whiteColor;
+        hamburger.style.color = 'darkBlueColor';
     }
 });
-const category = document.getElementsByClassName(
-    'navbar__navigationitem--heading',
-)[0];
-const dialogTab = document.getElementsByClassName('navbar__dialog--tablet')[0];
+const category = document.getElementById('category');
+const dialogTab = document.getElementById('dialogTab');
 category.addEventListener('click', () => {
     if (dialogTab.hasAttribute('hidden')) {
         dialogTab.removeAttribute('hidden');
-        category.style.background =
-            'linear-gradient(97.54deg, #002f52 35.49%, #326589 165.37%)';
-        category.style.color = 'white';
+        category.style.background = `linear-gradient(97.54deg, ${darkBlueColor} 35.49%, ${cyanBlueColor} 165.37%)`;
+        category.style.color = whiteColor;
     } else {
         dialogTab.setAttribute('hidden', true);
-        category.style.background = 'white';
-        category.style.color = '#002f52';
+        category.style.background = whiteColor;
+        category.style.color = darkBlueColor;
     }
 });
