e06cf72e3a4bfddd0e79105e1b39506b7ba33d24
p94vanshgambhir <vansh.gambhir@joshtechnologygroup.com>
2026-08-11 23:58:41 +0530

    [VG_A1_03]: did fixes as suggested

--- src/script/index.js
+++ src/script/index.js
@@ -1,16 +1,10 @@
 const hamburger = document.getElementById('hamburger');
 const dialogMobile = document.getElementById('dialog_mobile');
-const whiteColor = 'white';
-const darkBlueColor = '#002f52';
-const cyanBlueColor = '#326589';
 hamburger.addEventListener('click', () => {
     if (dialogMobile.hasAttribute('hidden')) {
         dialogMobile.removeAttribute('hidden');
-        hamburger.style.color = whiteColor;
     } else {
         dialogMobile.setAttribute('hidden', 'true');
-        hamburger.style.background = whiteColor;
-        hamburger.style.color = 'darkBlueColor';
     }
 });
 const category = document.getElementById('category');
@@ -18,11 +12,9 @@ const dialogTab = document.getElementById('dialogTab');
 category.addEventListener('click', () => {
     if (dialogTab.hasAttribute('hidden')) {
         dialogTab.removeAttribute('hidden');
-        category.style.background = `linear-gradient(97.54deg, ${darkBlueColor} 35.49%, ${cyanBlueColor} 165.37%)`;
-        category.style.color = whiteColor;
+        category.classList.add('navbar__dialog--open');
     } else {
         dialogTab.setAttribute('hidden', true);
-        category.style.background = whiteColor;
-        category.style.color = darkBlueColor;
+        category.classList.remove('navbar__dialog--open');
     }
 });
