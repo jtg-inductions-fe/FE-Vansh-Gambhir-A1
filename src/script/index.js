948dc308201d994c0d605dcd5291e2dc4257a7d1
p94vanshgambhir <vansh.gambhir@joshtechnologygroup.com>
2026-08-12 08:32:25 +0530

    Vg a1 03/carousel section (#6)
    * [VG_A1_01]: made navbar functional
    
    * [VG_A1_01]: fixes on navbar, implemented using bem model
    
    * [VG_A1_03]: Made latest and best section
    
    * [VG_A1_03]: removed unnecessary css import
    
    * [VG_A1_03]: fixed laptop height issue
    
    * [VG_A1_03]: Made changes to mobile
    
    * [VG_A1_01]: made navbar functional
    
    * [VG_A1_01]: fixes on navbar, implemented using bem model
    
    * [VG_A1_03]: Made latest and best section
    
    * [VG_A1_03]: removed unnecessary css import
    
    * [VG_A1_03]: fixed laptop height issue
    
    * [VG_A1_03]: Made changes to mobile
    
    * [VG_A1_04]: Made recently visited section (#7)
    
    * [VG_A1_03]: Made recently visited section
    
    * [VG_A1_05]: Made email section (#8)
    
    * [VG_A1_05]: Made email section
    
    * [VG_A1_06]: Made footer section (#9)
    
    * [VG_A1_06]: Made footer section
    
    * [VG_A1_06]: Fixed footer section display for mobile
    
    * [VG_A1_03]: Fixed conflict
    
    * [VG_A1_03]: did fixes as suggested

--- src/script/index.js
+++ src/script/index.js
@@ -1,32 +1,20 @@
-const hamburger = document.getElementsByClassName(
-    'navbar__hamburger--open-dialog',
-)[0];
-const dialogMobile = document.getElementsByClassName(
-    'navbar__dialog--mobile',
-)[0];
+const hamburger = document.getElementById('hamburger');
+const dialogMobile = document.getElementById('dialog_mobile');
 hamburger.addEventListener('click', () => {
     if (dialogMobile.hasAttribute('hidden')) {
         dialogMobile.removeAttribute('hidden');
-        hamburger.style.color = 'white';
     } else {
         dialogMobile.setAttribute('hidden', 'true');
-        hamburger.style.background = 'white';
-        hamburger.style.color = '#002f52';
     }
 });
-const category = document.getElementsByClassName(
-    'navbar__navigationitem--heading',
-)[0];
-const dialogTab = document.getElementsByClassName('navbar__dialog--tablet')[0];
+const category = document.getElementById('category');
+const dialogTab = document.getElementById('dialogTab');
 category.addEventListener('click', () => {
     if (dialogTab.hasAttribute('hidden')) {
         dialogTab.removeAttribute('hidden');
-        category.style.background =
-            'linear-gradient(97.54deg, #002f52 35.49%, #326589 165.37%)';
-        category.style.color = 'white';
+        category.classList.add('navbar__dialog--open');
     } else {
         dialogTab.setAttribute('hidden', true);
-        category.style.background = 'white';
-        category.style.color = '#002f52';
+        category.classList.remove('navbar__dialog--open');
     }
 });
